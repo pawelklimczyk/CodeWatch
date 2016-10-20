f4844e6d8c74c564d210f7ad121cd20756a8735d
Paweł Sołtysiak <pawel@soltysiak.it>
2016-10-20 21:22:33 +0200

    Adds new feature MaxMethodParameters

---
+++ src/Gmtl.CodeWatch/MaxMethodParametersWatcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gmtl.CodeWatch
+{
+    /// <summary>
+    /// Checks if Type contains methods with parameters count more than given.
+    /// </summary>
+    public class MaxMethodParametersWatcher : AbstractWatcher
+    {
+        private int maxParametersInMethod = 10;
+
+        private readonly BindingFlags searchFlags = BindingFlags.Public | BindingFlags.DeclaredOnly |
+                                                    BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+        protected override void CheckType(Type type)
+        {
+            foreach (MethodInfo methodInfo in type.GetMethods(searchFlags))
+            {
+                var parameter = methodInfo.GetParameters();
+
+                if (parameter.Length > maxParametersInMethod)
+                {
+                    throw new CodeWatchException(
+                        $"Method {type.FullName}.{methodInfo.Name} does not meet MaxMethodParametersWatcher standards. Parameter limit for method is {this.maxParametersInMethod}");
+                }
+            }
+        }
+
+        public void Configure(int newMax)
+        {
+            this.maxParametersInMethod = newMax;
+        }
+    }
+}
