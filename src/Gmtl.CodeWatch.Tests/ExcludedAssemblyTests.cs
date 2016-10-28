b0415ebdb29f61db6d12ea84adb4ea2598356a37
Pawel Klimczyk <pawel@klimczyk.pl>
2016-10-28 07:35:07 +0200

    Added tests for AssemblySkip in Watcher class

---
+++ src/Gmtl.CodeWatch.Tests/ExcludedAssemblyTests.cs
@@ -0,0 +1,54 @@
+﻿using Gmtl.CodeWatch.Watchers;
+using NUnit.Framework;
+
+namespace Gmtl.CodeWatch.Tests
+{
+    [TestFixture]
+    public class ExcludedAssemblyTests
+    {
+        private FieldNamingWatcher sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new FieldNamingWatcher();
+        }
+
+        [Test]
+        public void FieldNamingWatcherLowerCaseCheck_providedAssemblyWithUpperCasePropertiesToBeSkipped_ShouldPass()
+        {
+            sut.Configure(Naming.LowerCase);
+            sut.SkipAssembly(typeof(Gmtl.CodeWatch.TestData.AllUppercaseProperties.Class1).Assembly);
+
+            sut.Execute();
+
+            Assert.That(sut.Issues.Count, Is.EqualTo(0));
+        }
+        
+        [Test]
+        public void FieldNamingWatcherLowerCaseCheck_providedAssemblyWithUpperCasePropertiesToBeWatchedAndSkipped_ShoudlPass()
+        {
+            var assembly = typeof(Gmtl.CodeWatch.TestData.AllUppercaseProperties.Class1).Assembly;
+            sut.Configure(Naming.LowerCase);
+            sut.WatchAssembly(assembly);
+            sut.SkipAssembly(assembly);
+
+            sut.Execute();
+
+            Assert.That(sut.Issues.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FieldNamingWatcherLowerCaseCheck_providedAssemblyWithUpperCasePropertiesToBeSkippedAndWatched_ShoudlFail()
+        {
+            var assembly = typeof(Gmtl.CodeWatch.TestData.AllUppercaseProperties.Class1).Assembly;
+            sut.Configure(Naming.LowerCase);
+            sut.SkipAssembly(assembly);
+            sut.WatchAssembly(assembly);
+
+            sut.Execute();
+
+            Assert.That(sut.Issues.Count, Is.GreaterThan(0));
+        }
+    }
+}
