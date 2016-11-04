b45bdcc4500b1f63f55329f9e55b606ce359bde0
Pawel Klimczyk <pawel@klimczyk.pl>
2016-11-04 20:35:25 +0100

    Added MaxConstructorParametersWatcher

---
+++ src/Gmtl.CodeWatch.Tests/MaxConstructorParametersWatcherTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Gmtl.CodeWatch.Watchers;
+using NUnit.Framework;
+
+namespace Gmtl.CodeWatch.Tests
+{
+    public class MaxConstructorParametersWatcherTests
+    {
+        private MaxConstructorParametersWatcher sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new MaxConstructorParametersWatcher();
+        }
+
+        [TestCase(typeof(ClassWithNoParametersInConstructor))]
+        [TestCase(typeof(ClassWith1ParametersInConstructor))]
+        [TestCase(typeof(ClassWith3ParametersInConstructor))]
+        public void MaxConstructorParametersWatcher_providedClassShouldPassWithParametersBelow5(Type type)
+        {
+            sut.Configure(4);
+            sut.WatchType(type);
+
+            sut.Execute();
+
+            Assert.That(sut.Issues.Count, Is.EqualTo(0));
+        }
+
+        [TestCase(typeof(ClassWith5ParametersInConstructor))]
+        public void MaxConstructorParametersWatcher_providedClassShouldFailWith5Parameters(Type type)
+        {
+            sut.Configure(4);
+            sut.WatchType(type);
+
+            sut.Execute();
+
+            Assert.That(sut.Issues.Count, Is.GreaterThan(0));
+        }
+
+        #region test classes
+        class ClassWithNoParametersInConstructor { }
+
+        class ClassWith1ParametersInConstructor
+        {
+            public int A;
+
+            public ClassWith1ParametersInConstructor(int a)
+            {
+                A = a;
+            }
+        }
+
+        class ClassWith3ParametersInConstructor
+        {
+            public int A, B, C;
+
+            public ClassWith3ParametersInConstructor(int a, int b, int c)
+            {
+                A = a;
+                B = b;
+                C = c;
+            }
+        }
+
+        class ClassWith5ParametersInConstructor
+        {
+            public int A, B, C, D, E;
+
+            public ClassWith5ParametersInConstructor(int a, int b, int c, int d, int e)
+            {
+                A = a;
+                B = b;
+                C = c;
+                D = d;
+                E = e;
+            }
+        }
+        #endregion
+    }
+}
---
+++ src/Gmtl.CodeWatch/Watchers/MaxConstructorParametersWatcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace Gmtl.CodeWatch.Watchers
+{
+    public class MaxConstructorParametersWatcher : AbstractWatcher
+    {
+        private int maxParametersInConstroctur = 4;
+
+        private readonly BindingFlags searchFlags = BindingFlags.Public | BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Static;
+
+        public MaxConstructorParametersWatcher(CodeWatcherContext context = null) : base(context) { }
+
+        protected override void CheckType(Type type)
+        {
+           
+            foreach (ConstructorInfo constructorInfo in type.GetConstructors(searchFlags))
+            {
+                var parameter = constructorInfo.GetParameters();
+
+                if (parameter.Length > maxParametersInConstroctur)
+                {
+                    AddIssue(new CodeWatchException(String.Format("Constructor {0}.{1} does not meet MaxConstructorParametersWatcher standards. Parameter limit for constructor is {2}", type.FullName, constructorInfo.Name, maxParametersInConstroctur)));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Confugure how much maximum parameters can constructor
+        /// </summary>
+        /// <param name="newMaxParametersCount">max number of parameters</param>
+        /// <returns>watcher</returns>
+        public AbstractWatcher Configure(int newMaxParametersCount)
+        {
+            maxParametersInConstroctur = newMaxParametersCount;
+
+            return this;
+        }
+    }
+}
