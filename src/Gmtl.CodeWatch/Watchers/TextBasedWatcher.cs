16abc9b6a7b2e94ad5b0f9da77329e94219055d8
Pawel Klimczyk <pawel@klimczyk.pl>
2016-11-28 16:06:03 +0100

    Introduced TextBasedWatcher

---
+++ src/Gmtl.CodeWatch/Watchers/TextBasedWatcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Gmtl.CodeWatch.Watchers
+{
+    class TextBasedWatcher : AbstractWatcher
+    {
+        private Regex matcher;
+        
+        public TextBasedWatcher(CodeWatcherContext context) : base(context)
+        {
+        }
+
+        protected override void CheckType(Type type)
+        {
+        }
+    }
+}
